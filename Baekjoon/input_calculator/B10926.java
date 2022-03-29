f02548ca606daff48a838c340c18b39830a7f8f6
zoeyourlife <201814112@office.skhu.ac.kr>
2022-03-30 04:12:26 +0900

    Input_calcu

---
+++ Baekjoon/input_calculator/B10926.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B10926 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.nextLine();
+		
+		System.out.println(str+"??!");
+		
+		sc.close();
+
+	}
+
+}
