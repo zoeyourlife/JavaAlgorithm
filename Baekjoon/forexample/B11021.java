de371e0fc3fd1d744ed9d790693b9e36270fe72f
zoeyourlife <201814112@office.skhu.ac.kr>
2022-04-01 20:23:18 +0900

    forEx

---
+++ Baekjoon/forexample/B11021.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B11021 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int t = sc.nextInt();
+
+		for (int i = 1; i <= t; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int sum = a+b;
+			System.out.println("Case #" + i + ": " + sum);
+		}
+
+		sc.close();
+
+	}
+
+}
---
+++ Baekjoon/forexample/B2742.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B2742 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = n; i > 0; i--) {
+			System.out.println(i);
+		}
+
+		sc.close();
+
+	}
+
+}
