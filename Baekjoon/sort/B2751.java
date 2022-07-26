70e856b19dae3be7913b04e70b12315054b88ee3
zoeyourlife <201814112@office.skhu.ac.kr>
2022-07-26 23:35:56 +0900

    정렬2

---
+++ Baekjoon/sort/B2751.java
@@ -0,0 +1,30 @@
+package sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class B2751 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+
+		int n = sc.nextInt();
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			list.add(sc.nextInt());
+		}
+
+		Collections.sort(list);
+
+		for (int value : list) {
+			sb.append(value).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
